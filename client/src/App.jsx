b3643897f9c679dd5163f45fbdbb1449e7ddd2e3
David Goodwin <dgoodwin326@gmail.com>
2023-09-12 17:57:27 +0300

    add basic routing; create Layout and IndexPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import IndexPage from "./pages/IndexPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [{
+            index: true,
+            element: <IndexPage />
+        }]
+    },
+]);
+
+export default function App() {
     return (
-        <>Status page</>
+        <div className="app">
+            <RouterProvider router={router} />
+        </div>
     );
 }
-
-export default App;
