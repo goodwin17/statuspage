db511d25881627dff64ef5c9320ed950ee93423b
David Goodwin <dgoodwin326@gmail.com>
2023-09-16 18:44:48 +0300

    add utils.py; create check service functions

---
+++ server/utils.py
@@ -0,0 +1,29 @@
+import requests
+import platform
+import subprocess
+from time import time
+
+def check_service_http(url):
+    response = requests.get(url)
+    result = {
+        'status_code': response.status_code,
+        'response_time': response.elapsed
+    }
+    
+    return result
+
+def check_service_icmp(host):
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+    result = {}
+
+    try:
+        start_time = time()
+        subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        end_time = time()
+        result['response_time'] = (end_time - start_time) * 1000
+        result['status'] = 'ok'
+    except:
+        result['status'] = 'no response'
+    
+    return result
