ad563b1272bb9e0b866b6a7f16d035a0bca4e0ce
David Goodwin <dgoodwin326@gmail.com>
2023-09-17 05:03:16 +0300

    add jwt; create api login endpoint

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, request, jsonify
 from models import *
+from flask_jwt_extended import JWTManager, create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 @app.route("/")
 def index():
@@ -31,6 +34,24 @@ def api_incidents_service_id():
 @app.route("/api/checks") # 'service' parameter is necessary
 def api_checks_service_id():
     return "Checks here"
+
+@app.route("/api/login", methods=["POST"])
+def api_login():
+    login = request.json.get("login")
+    password = request.json.get("password")
+    user = db.session.query(User).filter_by(login=login).one_or_none()
+
+    if user is None:
+        return jsonify({ "message": "no such user" }), 401
+
+    password_hash = generate_password_hash(password + user.password_salt)
+
+    if password_hash != user.password_hash:
+        return jsonify({ "message": "wrong password" }), 401
+    
+    token = create_access_token(login) # todo
+
+    return jsonify({ "token": token })
 
 if __name__ == "__main__":
     app.run(debug=True)
