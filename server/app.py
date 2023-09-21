ac119bb5a8ce1fdc45133ae87ec5ab28a4843ecd
David Goodwin <dgoodwin326@gmail.com>
2023-09-22 01:54:26 +0300

    move configuration to another file

--- server/app.py
+++ server/app.py
@@ -7,8 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["JWT_SECRET_KEY"] = "secret"
+app.config.from_pyfile("config.py")
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
--- server/config.py
+++ server/config.py
@@ -1,1 +1,8 @@
-# there will be configuration of the app here
+from os import getenv
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), ".env"))
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+JWT_SECRET_KEY = getenv("JWT_SECRET_KEY")
