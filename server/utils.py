44faf303c78c1686ac35c13a93ae7ad226fca590
David Goodwin <dgoodwin326@gmail.com>
2023-09-27 04:25:10 +0300

    add service monitoring functionality

--- server/utils.py
+++ server/utils.py
@@ -2,6 +2,7 @@ import requests
 import platform
 import subprocess
 from time import time
+import threading
 
 
 def check_service_http(url):
@@ -32,3 +33,28 @@ def check_service_icmp(host):
 
 def serialize(obj):
     return {c.name: getattr(obj, c.name) for c in obj.__table__.columns}
+
+
+def set_service_monitoring(url, interval):
+    while True:
+        check_thread = threading.Thread(target=check_service_http, args=(url,))
+        print(f'check started at {url}')
+        check_thread.daemon = True
+        check_thread.start()
+        time.sleep(interval)
+
+
+def monitor(websites):
+    threads = []
+    
+    for website in websites:
+        url = website["url"]
+        interval = website["interval"]
+        print(f"Monitoring {url} every {interval} seconds.")
+        thread = threading.Thread(target=set_service_monitoring, args=(url, interval))
+        thread.daemon = True
+        thread.start()
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.join()
