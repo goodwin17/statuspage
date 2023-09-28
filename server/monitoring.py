f925627193408e05f68beba37f9563f9d8b5cb35
David Goodwin <dgoodwin326@gmail.com>
2023-09-28 05:02:57 +0300

    add monitoring class

---
+++ server/monitoring.py
@@ -0,0 +1,40 @@
+from jobs import check_service
+
+
+class Monitoring:
+    def __init__(self, scheduler, services):
+        self.scheduler = scheduler
+        self.services = services
+
+
+    def setup(self):
+        for service in self.services:
+            self.add(service)
+        
+        self.scheduler.start()
+
+
+    def add(self, service):
+        if not self.__has_service(service['id']):
+            self.scheduler.add_job(
+                id=str(service['id']),
+                func=check_service,
+                args=[service],
+                trigger='interval',
+                seconds=service['check_interval']
+            )
+            print('added')
+
+
+    def start(self, service_id):
+        if self.__has_service(service_id):
+            self.scheduler.resume_job(service_id)
+
+
+    def stop(self, service_id):
+        if self.__has_service(service_id):
+            self.scheduler.pause_job(service_id)
+
+
+    def __has_service(self, id):
+        return any(service['id'] == id for service in self.services)
