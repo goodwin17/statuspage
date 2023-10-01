3d4c0e6d0b62c061b371ab0a2d307c31d19c3ece
David Goodwin <dgoodwin326@gmail.com>
2023-10-01 05:05:57 +0300

    clean file; apply refactored monitor

--- server/app.py
+++ server/app.py
@@ -1,26 +1,19 @@
 from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, unset_jwt_cookies,\
     jwt_required, set_access_cookies, verify_jwt_in_request,\
     get_jwt_identity, create_refresh_token, set_refresh_cookies
 from flask_apscheduler import APScheduler
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from utils import serialize, serialize_all, convert_dict_notation
 
 app = Flask(__name__)
-
 app.config.from_pyfile("config.py")
-
-from models import db, User, Service, Incident
-from monitoring import Monitoring
-from utils import serialize, serialize_all
-
-with app.app_context():
-    services = serialize_all(db.session.query(Service).all())
-    monitoring = Monitoring(APScheduler(), services)
-    monitoring.setup()
-
 jwt = JWTManager(app)
+
+from monitor import Monitor
+from models import db, User, Service, Incident
+
+service_monitor = Monitor(APScheduler(app=app))
 
 
 @app.route("/")
@@ -39,11 +32,11 @@ def api_protected():
     return "Protected here"
 
 
-@app.route("/api/services", methods=["GET", "POST"]) # get all services
+@app.route("/api/services", methods=["GET", "POST"])
 def api_services():
     if request.method == "GET":
         services = db.session.query(Service).all()
-        return jsonify([serialize(service) for service in services])
+        return jsonify(serialize_all(services))
     
     verify_jwt_in_request()
     data = request.get_json()
@@ -57,19 +50,19 @@ def api_services():
 
     try:
         db.session.add(service)
+        db.session.commit()
     except:
         return jsonify({ "msg": "service already exist" }), 409
     
-    db.session.commit()
-    monitoring.add(serialize(service))
+    service_monitor.add(service)
     return jsonify({ "msg": "service added" })
 
 
 @app.route("/api/services/<id>", methods=["GET", "PUT", "DELETE"])
 def api_services_id(id):
     data = request.get_json()
-    service_query = db.session.query(Service).filter_by(id=id)
-    service = service_query.one_or_none()
+    service_query_filter = db.session.query(Service).filter_by(id=id)
+    service = service_query_filter.one_or_none()
     
     if service is None:
         return jsonify({ "msg": "no service with such id" }), 400
@@ -81,27 +74,38 @@ def api_services_id(id):
     
     if request.method == "PUT":
         try:
-            current_status = service['monitoring_status']
-            service_query.update(data)
+            service_query_filter.update(convert_dict_notation(data))
             db.session.commit()
-
-            if current_status != data['monitoring_status']:
-                if data['monitoring_status'] == 1:
-                    monitoring.start(id)
-                else:
-                    monitoring.stop(id)
-        except:
-            return jsonify({ "msg": "can not update service" })
+            service_monitor.update(service_query_filter.first())
+        except Exception as e:
+            return jsonify({ "msg": "can not update service", "error": str(e) })
         
         return jsonify({ "msg": "service updated" })
     
     try:
-        service_query.delete()
+        service_query_filter.delete()
+        db.session.commit()
     except:
         return jsonify({ "msg": "can not delete service" })
     
-    db.session.commit()
     return jsonify({ "msg": "service deleted" })
+
+
+@app.route("/api/services/<id>/change-status", methods=["POST"])
+@jwt_required()
+def api_services_id_change_status(id):
+    service = db.session.query(Service).filter_by(id=id).first()
+    
+    if service.monitoring_status == 1:
+        service.monitoring_status = 0
+        db.session.commit()
+        service_monitor.stop(id)
+    else:
+        service.monitoring_status = 1
+        db.session.commit()
+        service_monitor.start(id)
+    
+    return jsonify({ "msg": "service monitoring status changed" })
 
 
 @app.route("/api/incidents", methods=["GET"]) # 'service-id' parameter is necessary
@@ -112,10 +116,10 @@ def api_incidents_service_id():
         return jsonify({ "msg": "no service-id url argument" }), 400
     
     incidents = db.session.query(Incident).filter_by(service_id=service_id).all()
-    return jsonify([serialize(incident) for incident in incidents])
+    return jsonify(serialize_all(incidents))
 
 
-@app.route("/api/checks") # 'service' parameter is necessary
+@app.route("/api/checks") # 'service-id' parameter is necessary
 def api_checks_service_id():
     return "Checks here"
 
@@ -131,7 +135,7 @@ def api_login():
     if not check_password_hash(user.password_hash, data["password"]):
         return jsonify({ "msg": "wrong password" }), 401
     
-    access_token = create_access_token(user.login) # todo
+    access_token = create_access_token(user.login)
     refresh_token = create_refresh_token(user.login)
     response = make_response({ "msg": "successfully logged in" })
     set_access_cookies(response, access_token)
@@ -159,7 +163,6 @@ def api_register():
         password_hash=generate_password_hash(data["password"], salt_length=64),
         role=data["role"]
     )
-    
     db.session.add(new_user)
     db.session.commit()
     return jsonify({ "msg": "user added" })
@@ -176,4 +179,8 @@ def api_refresh():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() # create tables if some tables don't exist
+        service_monitor.run()
+
     app.run(debug=True, use_reloader=False)
