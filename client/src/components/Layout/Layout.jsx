7ca1a7e5c6e8531ca47ac5a729c62c5eb8b8a327
David Goodwin <dgoodwin326@gmail.com>
2023-10-02 04:51:41 +0300

    correct Layout component

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "@components/Header";
+import Main from "@components/Main";
+import Footer from "@components/Footer";
 
 export default function Layout() {
 	return (
 		<>
-			<Outlet />
+			<Header />
+			<Main>
+				<Outlet />
+			</Main>
+			<Footer />
 		</>
 	);
 }
