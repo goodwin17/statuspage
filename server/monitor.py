f2b583de78536cfdd072ffadf1deb3ba948a51a7
David Goodwin <dgoodwin326@gmail.com>
2023-10-02 05:11:42 +0300

    add incident registration functionality

--- server/monitor.py
+++ server/monitor.py
@@ -1,6 +1,6 @@
 from __main__ import app
 from flask_apscheduler import APScheduler
-from models import db, Service, Check
+from models import db, Service, Check, Incident
 from utils import check_service_http, check_service_icmp
 from datetime import datetime
 import json
@@ -8,11 +8,13 @@ import json
 class Monitor:
     def __init__(self):
         self.scheduler = APScheduler(app=app)
+        self.last_statuses = {}
 
     
     def run(self):
         for service in self.services():
             self.add(service)
+            self.last_statuses[service.id] = None
         
         self.scheduler.start()
 
@@ -25,6 +27,7 @@ class Monitor:
             trigger='interval',
             seconds=service.check_interval
         )
+        self.last_statuses[service.id] = None
     
 
     def start(self, service_id):
@@ -37,6 +40,7 @@ class Monitor:
 
     def remove(self, service_id):
         self.scheduler.remove_job(str(service_id))
+        del self.last_statuses[service_id]
 
     
     def update(self, service): # method, interval
@@ -60,16 +64,16 @@ class Monitor:
         if service is None:
             return None
         
-        method = None
         result = None
+        method_func = None
         current_time = str(datetime.now())
 
         if service.check_method == 'http':
-            method = check_service_http
+            method_func = check_service_http
         else:
-            method = check_service_icmp
+            method_func = check_service_icmp
         
-        result = method(service.address)
+        result = method_func(service.address)
 
         if result['status'] == 'error': # either 'ok' or 'error'
             return None
@@ -79,6 +83,34 @@ class Monitor:
             datetime=str(current_time),
             result=json.dumps(result)
         )
+
         with app.app_context():
             db.session.add(check)
             db.session.commit()
+        
+        last_status = self.last_statuses[service_id]
+
+        if last_status is None or last_status != result['status']:
+            self.register_incident(service_id, last_status, result['status'], current_time)
+
+
+
+    def register_incident(self, service_id, last_check_status, current_check_status, current_time):
+        title = None
+
+        if last_check_status is None:
+            title = "Monitoring started"
+        else:
+            title = "Down right now"
+        
+        incident = Incident(
+            service_id=service_id,
+            title=title,
+            datetime=str(current_time)
+        )
+        
+        with app.app_context():
+            db.session.add(incident)
+            db.session.commit()
+        
+        self.last_statuses[service_id] = current_check_status
