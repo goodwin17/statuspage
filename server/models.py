b17dfc087238e531bb9c7c896846eed87a33b2e2
David Goodwin <dgoodwin326@gmail.com>
2023-10-05 03:01:01 +0300

    server: create extended base Model class

--- server/models.py
+++ server/models.py
@@ -4,16 +4,41 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class ExtendedModel(db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+
+    @classmethod
+    def create(cls, **columns):
+        try:
+            obj = cls(columns)
+            db.session.add(obj)
+            db.session.commit()
+        except Exception as e:
+            raise e
+        
+        return obj
+    
+    def update(self, **columns):
+        try:
+            for key, value in columns.items():
+                setattr(self, key, value)
+            
+            db.session.commit()
+        except Exception as e:
+            raise e
+
+        return self
+
+
+class User(ExtendedModel):
     name = db.Column(db.Text, nullable=False)
     login = db.Column(db.Text, unique=True, nullable=False)
     password_hash = db.Column(db.Text, nullable=False)
     role = db.Column(db.Text, nullable=False)
 
 
-class Service(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+class Service(ExtendedModel):
     name = db.Column(db.Text, unique=True, nullable=False)
     address = db.Column(db.Text, unique=True, nullable=False)
     check_method = db.Column(db.Text, nullable=False)
@@ -23,15 +48,13 @@ class Service(db.Model):
     incidents = db.relationship("Incident", backref="service")
 
 
-class Check(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+class Check(ExtendedModel):
     service_id = db.Column(db.Integer, db.ForeignKey("service.id"))
     datetime = db.Column(db.Text, nullable=False)
     result = db.Column(db.Text) # JSON with status, code and response_time
 
 
-class Incident(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+class Incident(ExtendedModel):
     service_id = db.Column(db.Integer, db.ForeignKey("service.id"))
     type = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
