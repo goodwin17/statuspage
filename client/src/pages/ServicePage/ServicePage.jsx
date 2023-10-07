6612d1d0f0e849163b6f51c1e13f9608a5093603
David Goodwin <dgoodwin326@gmail.com>
2023-10-07 04:32:01 +0300

    move test data to service page

--- client/src/pages/ServicePage/ServicePage.jsx
+++ client/src/pages/ServicePage/ServicePage.jsx
@@ -1,7 +1,60 @@
+import PageTitle from "@components/PageTitle";
+import DataSection from "@components/DataSection";
+import DataStack from "@components/DataStack";
+import IncidentList from "@components/IncidentList";
+
+const incidents = [{
+    id: 1,
+    type: 'up',
+    title: "Running again",
+    reason: "Some reason",
+    datetime: "some date"
+}, {
+    id: 2,
+    type: 'down',
+    title: "Down right now",
+    reason: "Some reason",
+    datetime: "some date"
+}];
+
+const overallUptimeData = [{
+    capture: 'Last day',
+    value: '99%',
+}, {
+    capture: 'Last 7 days',
+    value: '98.23%'
+}, {
+    capture: 'Last 30 days',
+    value: '97.456%'
+},];
+
+const service = {
+    name: 'Example website',
+    address: 'https://example.com',
+    checkMethod: 'http',
+    checkInterval: 60,
+    monitoringStatus: 1
+};
+
 export default function ServicePage() {
     return (
         <>
-            Service page
+            <PageTitle
+                title={`${service.name} is operational`}
+                details={`
+                    Being checked every ${service.checkInterval} minutes
+                    by ${service.checkMethod.toUpperCase()} method
+                `}
+            />
+            <DataSection title="Test" details="Something">
+                Test page
+            </DataSection>
+            <DataSection title="Recent incidents">
+                <IncidentList incidents={incidents} />
+            </DataSection>
+            <DataSection title="Overall uptime">
+                <DataStack dataItems={overallUptimeData} />
+            </DataSection>
         </>
     );
 }
