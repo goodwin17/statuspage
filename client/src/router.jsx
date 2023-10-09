797681b6a2bcedc5b3e3b86fab1749ade5794e40
David Goodwin <dgoodwin326@gmail.com>
2023-10-10 02:58:23 +0300

    add id param to service route

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,33 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@components/Layout";
 import IndexPage from "@pages/IndexPage";
-import ServicePage, { loader as servicePageLoader } from "@pages/ServicePage";
+import ServicePage, { loader as servicePageLoader} from "@pages/ServicePage";
 import LoginPage from "@pages/LoginPage";
 import AdminPage from "@pages/IndexPage";
 import TestPage from "@pages/TestPage";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [{
-            index: true,
-            element: <IndexPage />
-        }, {
-            path: "service",
-            loader: servicePageLoader,
-            element: <ServicePage />
-        }, {
-            path: "login",
-            element: <LoginPage />
-        }, {
-            path: "admin",
-            element: <AdminPage />
-        }, {
-            path: "test",
-            element: <TestPage />
-        }]
-    },
-]);
+const router = createBrowserRouter([{
+    path: "/",
+    element: <Layout />,
+    children: [{
+        index: true,
+        element: <IndexPage />
+    }, {
+        path: "services/:serviceId",
+        element: <ServicePage />,
+        loader: servicePageLoader
+    }, {
+        path: "login",
+        element: <LoginPage />
+    }, {
+        path: "admin",
+        element: <AdminPage />
+    }, {
+        path: "test",
+        element: <TestPage />
+    }]
+}]);
 
 export default router;
