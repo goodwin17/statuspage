28fae8059608b1d739f0840b6fb5b68e55a3c5e3
David Goodwin <dgoodwin326@gmail.com>
2023-10-10 02:59:20 +0300

    correct getService request

--- client/src/helpers/requests.jsx
+++ client/src/helpers/requests.jsx
@@ -3,10 +3,16 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://127.0.0.1:5000/api';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-const getService = async (serviceId) => {
-    let service = await axios.get(`/services/${serviceId}`);
-    console.log(service.data);
-    return service;
-};
+async function getService(serviceId) {
+    console.log("getService start");
+    let response = await axios.get(`/services/${serviceId}`).catch((error) => console.log(error));
+    console.log("getService end");
+
+    if (response) {
+        return response.data;
+    }
+
+    return null;
+}
 
 export { getService };
