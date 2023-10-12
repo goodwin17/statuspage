d6280934fb5ded6a020af390f87ad450d115b6fb
David Goodwin <dgoodwin326@gmail.com>
2023-10-12 05:28:12 +0300

    refactor monitor: add enum checks

--- server/monitor.py
+++ server/monitor.py
@@ -1,9 +1,16 @@
 from __main__ import app
 from flask_apscheduler import APScheduler
-from models import db, Service, Check, Incident
+from models import db, Service, Check, Incident, IncidentType, CheckMethod
 from utils import check_service_http, check_service_icmp
 from datetime import datetime
 import json
+from enum import Enum
+
+class CheckStatus(Enum):
+    OK = "ok"
+    ERROR = "error"
+    TIMEOUT = "timeout"
+
 
 class Monitor:
     def __init__(self):
@@ -66,21 +73,22 @@ class Monitor:
         
         result = None
         method_func = None
-        current_time = str(datetime.now())
+        # current_time = str(datetime.now())
+        current_time = datetime.now()
 
-        if service.check_method == "http":
+        if service.check_method == CheckMethod("http").name:
             method_func = check_service_http
         else:
             method_func = check_service_icmp
         
         result = method_func(service.address)
 
-        if result["status"] == "error": # either "ok" or "error"
-            return None
+        # if result["status"] == "error": # either "ok" or "error"
+        #     return None
         
         check = Check(
             service_id=service.id,
-            datetime=str(current_time),
+            datetime=current_time,
             result=json.dumps(result)
         )
 
@@ -92,20 +100,31 @@ class Monitor:
 
         if last_status is None or last_status != result["status"]:
             self.register_incident(service_id, last_status, result["status"], current_time)
+        
+        self.last_statuses[service_id] = result["status"]
 
 
     def register_incident(self, service_id, last_check_status, current_check_status, current_time):
         title = None
+        incident_type = None
 
-        if last_check_status is None:
+        if last_check_status is None or last_check_status != CheckStatus.OK:
+            incident_type = IncidentType.UP
             title = "Monitoring started"
-        else:
+        elif last_check_status == CheckStatus.OK:
+            incident_type = IncidentType.DOWN
             title = "Down right now"
+        
+        incident_type = IncidentType.get(type)
+
+        if incident_type is None:
+            return None
         
         incident = Incident(
             service_id=service_id,
+            type=incident_type,
             title=title,
-            datetime=str(current_time)
+            datetime=current_time
         )
         
         with app.app_context():
