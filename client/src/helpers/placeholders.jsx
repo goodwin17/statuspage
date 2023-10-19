bc22f986f7b3208fd6754cb40972710524424ae4
David Goodwin <dgoodwin326@gmail.com>
2023-10-19 03:07:52 +0300

    add uptime data placeholder

--- client/src/helpers/placeholders.jsx
+++ client/src/helpers/placeholders.jsx
@@ -13,15 +13,18 @@ let incidents = [{
 }];
 
 let overallUptime = [{
-    capture: 'Last day',
+    capture: 'Last 24 hours',
     value: '99%',
 }, {
     capture: 'Last 7 days',
     value: '98.23%'
 }, {
     capture: 'Last 30 days',
-    value: '97.456%'
-},];
+    value: '97.456%',
+}, {
+    capture: 'Last 60 days',
+    value: '99.995%'
+}];
 
 let service = {
     name: 'Example website',
@@ -31,8 +34,24 @@ let service = {
     monitoringStatus: 1
 };
 
+function generateUptimeData(size) {
+    let days = [];
+
+    for (let i = 0; i < size; i++) {
+        days.push({
+            date: `October ${i + 1}, 2023`,
+            value: Math.random() > 0.3 ? 100 : Math.round(Math.random() * 49 + 50)
+        });
+    }
+
+    return days;
+}
+
+let uptimeData = generateUptimeData(60);
+
 export {
     incidents,
     overallUptime,
-    service
+    service,
+    uptimeData
 };
