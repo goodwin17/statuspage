5eab45d77375e82089be1775b42bb2e3cbf2ac32
David Goodwin <dgoodwin326@gmail.com>
2023-10-19 03:11:23 +0300

    change test page content

--- client/src/pages/TestPage/TestPage.jsx
+++ client/src/pages/TestPage/TestPage.jsx
@@ -1,7 +1,32 @@
+import DataSection from "@components/DataSection";
 import PageTitle from "@components/PageTitle";
+import UptimeChart from "@components/UptimeChart";
+import { useState } from "react";
+import Typography from "@mui/material/Typography";
+import { uptimeData } from "@helpers/placeholders";
 
 export default function TestPage() {
+    let [uptimeDays, setUptimeDays] = useState(uptimeData);
+
     return (
-        <PageTitle variant="h1">This is test page</PageTitle>
+        <>
+            <PageTitle variant="h1">This is test page</PageTitle>
+            <DataSection title='Uptime'>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center'
+                }}>
+                    <Typography
+                        color='#00d200'
+                        fontSize='1.3rem'
+                        marginRight={2}
+                    >
+                        97.254%
+                    </Typography>
+                    <UptimeChart height="30px" uptimeDays={uptimeDays} />
+                </div>
+            </DataSection>
+        </>
     );
 }
