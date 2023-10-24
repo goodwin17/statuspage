afdf5f333e48ecdbc1f72b47cc148a3e0aa9ca1c
David Goodwin <dgoodwin326@gmail.com>
2023-10-24 20:14:02 +0300

    hotfix

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,7 +3,8 @@ import Layout from "@components/Layout";
 import IndexPage from "@pages/IndexPage";
 import ServicePage, { loader as servicePageLoader} from "@pages/ServicePage";
 import LoginPage from "@pages/LoginPage";
-import AdminPage from "@pages/IndexPage";
+import AdminPage from "@pages/AdminPage";
+import AdminAccountPage from "@pages/AdminAccountPage";
 import TestPage from "@pages/TestPage";
 
 const router = createBrowserRouter([{
