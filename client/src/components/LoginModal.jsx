1bb3f66906d36c3fd82e7b9d299337022c057e0e
David Goodwin <dgoodwin326@gmail.com>
2023-10-31 18:27:34 +0300

    replace mui modal component with own

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,36 +1,13 @@
-import { Modal, Paper, IconButton } from "@mui/material";
+import Modal from "@components/Modal";
 import LoginForm from "@components/LoginForm";
-import CloseIcon from '@mui/icons-material/Close';
 import useModal from "@hooks/useModal";
 
 export default function LoginModal() {
     const { isModalOpen, closeModal } = useModal();
 
     return (
-        <Modal
-        open={isModalOpen}
-        onClose={closeModal}
-        >
-            <Paper sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                paddingTop: 4,
-                paddingBottom: 4,
-                paddingLeft: 2,
-                paddingRight: 2,
-                borderRadius: 2
-            }}>
-                <LoginForm />
-                <IconButton onClick={closeModal} sx={{
-                    position: 'absolute',
-                    top: '0.5rem',
-                    right: '0.5rem'
-                }}>
-                    <CloseIcon />
-                </IconButton>
-            </Paper>
+        <Modal open={isModalOpen} onClose={closeModal}>
+            <LoginForm />
         </Modal>
-);
+    );
 }
