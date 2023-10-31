f365341e103b2617163987ebc7e728106b3616a5
David Goodwin <dgoodwin326@gmail.com>
2023-10-31 18:27:49 +0300

    refactor Loaded component

--- client/src/components/Loaded.jsx
+++ client/src/components/Loaded.jsx
@@ -1,13 +1,24 @@
 import { Skeleton } from "@mui/material";
 import { isObject } from "@helpers/utils";
 
-export default function Loaded({resolve, children, loader = <Skeleton height={'4rem'} />}) {
+export default function Loaded({
+    resolve,
+    children,
+    skeletonHeight = '3rem',
+    skeletonVariant = 'text',
+    loader = null
+}) {
     if (!isObject(resolve)) {
         return "Invalid 'resolve' parameter";
     }
 
-    if (Object.values(resolve).some(value => value === null)) {
-        return loader;
+    if (Object.values(resolve).some(value => !value)) {
+        return loader ?? (
+            <Skeleton
+                variant={skeletonVariant}
+                height={skeletonHeight}
+            />
+        );
     }
 
     if (typeof children !== 'function') {
