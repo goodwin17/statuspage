135f1034866ef0111b7ae4cffb61548e871e9365
David Goodwin <dgoodwin326@gmail.com>
2023-11-12 15:00:16 +0300

    refactor auth hook: add auth check

--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -1,9 +1,20 @@
 import AuthContext from "@contexts/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function useAuth() {
     let { checkAuth, ...auth } = useContext(AuthContext);
-    let [isAuth, user] = checkAuth();
+    const [isAuth, setIsAuth] = useState(null);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function getAuth() {
+            let [isAuth, user] = await checkAuth();
+            setIsAuth(isAuth);
+            setUser(user);
+        }
+
+        getAuth();
+    }, []);
     
     return {
         isAuth,
