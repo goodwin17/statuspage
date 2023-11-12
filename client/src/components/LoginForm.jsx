43f2847b6f92f16cfec031832998ed77a202dd96
David Goodwin <dgoodwin326@gmail.com>
2023-11-12 15:05:11 +0300

    refactor login form component

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,13 +1,9 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Button from "@components/Button";
+import FormBox from "@mui/material/FormBox";
+import FormTextField from "@components/FormTextField";
 import useAuth from "@hooks/useAuth";
-// import { useNavigate } from "react-router-dom";
-// import Button from "@mui/material/Button";
 
 export default function LoginForm() {
-    let { login } = useAuth();
+    let auth = useAuth();
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -16,52 +12,14 @@ export default function LoginForm() {
             login: formData.get('login'),
             password: formData.get('password')
         };
-        const loginSuccess = login(credentials);
+        const loginSuccess = auth.login(credentials);
         console.log(loginSuccess);
     }
     
     return (
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-                width: '360px',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 3,
-                padding: 1,
-                margin: '0 auto',
-            }}
-        >
-            <Typography
-                variant="h2"
-                align="center"
-            >
-                Log in as admin
-            </Typography>
-            <TextField
-                id="login"
-                name="login"
-                label="Login"
-                autoFocus
-                required
-            />
-            <TextField
-                id="password"
-                type="password"
-                name="password"
-                label="Password"
-                required
-            />
-            <Button
-                type="submit"
-                variant="contained"
-                sx={{
-                    fontSize: '1.1rem'
-                }}
-            >
-                Submit
-            </Button>
-        </Box>
+        <FormBox title="Log in as admin" onSubmit={handleSubmit}>
+            <FormTextField spec="login" autoFocus required />
+            <FormTextField spec="password" required />
+        </FormBox>
     );
 }
