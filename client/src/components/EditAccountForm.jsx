82578654d1460021896fa34a1aeec36f80b90aef
David Goodwin <dgoodwin326@gmail.com>
2023-11-12 15:07:54 +0300

    refactor edit account form

--- client/src/components/EditAccountForm.jsx
+++ client/src/components/EditAccountForm.jsx
@@ -1,12 +1,10 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Button from "@components/Button";
+import FormBox from "@mui/material/FormBox";
+import FormTextField from "@components/FormTextField";
 import useAuth from "@hooks/useAuth";
 import { editUser } from "@api/requests";
 
 export default function EditAccountForm() {
-    let { user } = useAuth();
+    let { isAuth, user } = useAuth();
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -19,57 +17,19 @@ export default function EditAccountForm() {
         const editSuccess = editUser(user.login, accountData);
         console.log(editSuccess);
     }
+
+    if (isAuth === null) {
+        return <>Loading...</>;
+    }
     
     return (
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-                width: '360px',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 3,
-                padding: 1,
-                margin: '0 auto',
-            }}
-        >
-            <Typography
-                variant="h2"
-                align="center"
-            >
-                Edit account
-            </Typography>
-            <TextField
-                id="name"
-                name="name"
-                label="name"
-                value={user.name}
-                autoFocus
-                required
+        <FormBox title="Edit account" onSubmit={handleSubmit}>
+            <FormTextField
+                spec="name" value={user.name}
+                autoFocus required
             />
-            <TextField
-                id="login"
-                name="login"
-                label="Login"
-                value={user.login}
-                required
-            />
-            <TextField
-                id="password"
-                type="password"
-                name="password"
-                label="Password"
-                required
-            />
-            <Button
-                type="submit"
-                variant="contained"
-                sx={{
-                    fontSize: '1.1rem'
-                }}
-            >
-                Submit
-            </Button>
-        </Box>
+            <FormTextField spec="login" value={user.login} required />
+            <FormTextField spec="password" required />
+        </FormBox>
     );
 }
