169fdb4a2c9547a8894e0cb23bbf6e5730d31a7e
David Goodwin <dgoodwin326@gmail.com>
2023-11-13 03:39:23 +0300

    reorder imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "@contexts/AuthContext.jsx";
+import router from "./router.jsx";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import router from "./router.jsx";
-import { AuthProvider } from "@contexts/AuthContext.jsx";
 
 export default function App() {
     return (
